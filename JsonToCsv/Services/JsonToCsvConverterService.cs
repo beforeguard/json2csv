ca31d1c05447de266b6fc95a13a8dc5d11ab08c5
Brian Egan <brianjoseph.egan@gmail.com>
2025-05-14 21:13:36 -0700

    Add implementation.

--- JsonToCsv/Services/JsonToCsvConverterService.cs
+++ JsonToCsv/Services/JsonToCsvConverterService.cs
@@ -1,10 +1,34 @@
-﻿namespace JsonToCsv.Services
+﻿using System.Text;
+using System.Text.Json;
+
+namespace JsonToCsv.Services
 {
     public class JsonToCsvConverterService
     {
         public string Convert(string json)
         {
-            throw new NotImplementedException();
+            var stringBuilder = new StringBuilder();
+
+            var document = JsonDocument.Parse(json);
+
+            if (document.RootElement.ValueKind == JsonValueKind.Array)
+            {
+                foreach (var element in document.RootElement.EnumerateArray())
+                {
+                    if (element.ValueKind == JsonValueKind.Object)
+                    {
+                        var properties = element.EnumerateObject().ToList();
+                        if (stringBuilder.Length == 0)
+                        {
+                            stringBuilder.AppendLine(string.Join(",", properties.Select(p => p.Name)));
+                        }
+
+                        stringBuilder.AppendLine(string.Join(",", properties.Select(p => p.Value.ToString())));
+                    }
+                }
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
